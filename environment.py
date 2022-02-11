d998b7e6faeab4e4af5252e95493c3d40c4532d6
Dimitris Michailidis <dimichai@outlook.com>
2022-02-11 17:16:35 +0100

    environment.py: now reads existing lines from config file

--- environment.py
+++ environment.py
@@ -1,5 +1,7 @@
 import configparser
 import torch
+import json
+import numpy as np
 
 device = torch.device('cpu')
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -30,6 +32,18 @@ def matrix_from_file(path, size_x, size_y):
 class Environment(object):
     """The City Grid environment the agent learns from."""
 
+    def grid_to_vector(self, grid_idx):
+        """Converts grid indices(x, y) to a vector index (x^).
+
+        Args:
+            grid_idx (torch.Tensor): the grid indices to be converted to vector indices: [[x1, y1], [x2, y2], ...]
+
+        Returns:
+            torch.Tensor: Converted vector.
+        """
+        v_idx = grid_idx[:, 0] * self.grid_x_size + grid_idx[:, 1]
+        return v_idx
+
     def __init__(self, path):
         """Initialise city environment.
 
@@ -43,12 +57,27 @@ class Environment(object):
         config.read(path / 'config.txt')
         assert 'config' in config
 
+        # todo delete this line
+        self.config_obj = config
+        #
+
         # size of the grid
         self.grid_x_size = config.getint('config', 'grid_x_size')
         self.grid_y_size = config.getint('config', 'grid_y_size')
         self.grid_size = self.grid_x_size * self.grid_y_size
         
-        # Build a grid_size x grid_size OD matrix.
+        # Build the OD and the SES matrices.
         self.od_mx = matrix_from_file(path / 'od.txt', self.grid_size, self.grid_size)
         self.price_mx = matrix_from_file(path / 'average_house_price_gid.txt', self.grid_x_size, self.grid_y_size)
+
+        # Read existing metro lines of the environment.
+        # json is used to load lists from ConfigParser as there is no built in way to do it.
+        existing_lines = json.loads(config.get('config', 'existing_lines'))
+        self.existing_lines = []
+        for line in existing_lines:
+            line = torch.Tensor(line).to(device)
+            # Convert grid indices (x,y) to vector indices (x^)
+            line = self.grid_to_vector(line)
+            self.existing_lines.append(line)
+            
         
