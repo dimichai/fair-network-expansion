b4b3a4ebfcdc9aa5592f39f9d216fc73b6f5b2ac
dimichai <dimitris@hey.com>
2022-03-30 13:24:21 +0200

    main.py: added num_layers and dropout arguments

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ if __name__ == "__main__":
     parser.add_argument('--hidden_size', default=128, type=int)
     parser.add_argument('--static_size', default=2, type=int)
     parser.add_argument('--dynamic_size', default=1, type=int)
+    parser.add_argument('--num_layers', default=1, type=int)
+    parser.add_argument('--dropout', default=0.1, type=float)
 
     args = parser.parse_args()
 
     # Prepare the environments.
     xian = Environment(Path('./environments/xian'))
 
-    trainer_xian = Trainer(xian, )
+    trainer_xian = Trainer(xian, args)
 
     print("made it!")
 
