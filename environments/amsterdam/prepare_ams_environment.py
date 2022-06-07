27a3cdb50902142f51389f7846fb2c2ce5c420f2
dimichai <dimitris@hey.com>
2022-06-07 16:45:27 +0200

    OD estimation based on universal mobility law

--- environments/amsterdam/prepare_ams_environment.py
+++ environments/amsterdam/prepare_ams_environment.py
@@ -7,18 +7,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #%%
-ams_nb = gpd.read_file('./ams-districts.geojson')
+ams_nb = gpd.read_file('./ams-districts.geojson', crs='EPSG:4326')
 # ams_nb = gpd.read_file('./ams-neighbourhoods.geojson')
 # For population/income data by buurt/wijk
 ams_ses = pd.read_csv('./ams-ds-ses.csv')
 ams_ses = ams_ses[['WK_CODE', 'pop', 'avg_inc_per_res']]
 
-# ams_nb = ams_nb.to_crs(epsg=3035)
+
+# Amersfoort / RD New -- Netherlands - Holland - Dutch
+CRS = 'EPSG:28992'
+# Flatten the neighborhoods
+ams_nb = ams_nb.to_crs(CRS)
 # %%
 xmin, ymin, xmax, ymax = ams_nb.total_bounds
 
-n = 45
-cell_size = (xmax-xmin)/n
+# n = 45
+# cell_size = (xmax-xmin)/n
+cell_size = 500
 
 cols = list(np.arange(xmin, xmax + cell_size, cell_size))
 rows = list(np.arange(ymin, ymax + cell_size, cell_size))
@@ -37,10 +42,7 @@ for i, y in enumerate(reversed(rows[:-1])):
         v += 1
         grid['geometry'].append(Polygon([(x,y), (x+cell_size, y), (x+cell_size, y+cell_size), (x, y + cell_size)]))
 
-# grid = gpd.GeoDataFrame({'geometry':polygons}, crs='EPSG:4326')
-grid = gpd.GeoDataFrame(grid)
-# grid = grid.to_crs('EPSG:4326')
-# grid = grid.to_crs('EPSG:3035')
+grid = gpd.GeoDataFrame(grid, crs=CRS)
 grid_x_size = grid.g_x.max() + 1
 grid_y_size = grid.g_y.max() + 1
 
@@ -53,10 +55,9 @@ metro_stops = metro_stops[metro_stops['Modaliteit'] == 'Metro']
 metro_stops = gpd.GeoDataFrame(
                 metro_stops, 
                 geometry=gpd.points_from_xy(metro_stops['LNG'], 
-                metro_stops['LAT']))
-
-# metro_stops = metro_stops.to_crs('EPSG:4326')
-# metro_stops = metro_stops.to_crs(epsg=3035)
+                metro_stops['LAT']),
+                crs='EPSG:4326')
+metro_stops = metro_stops.to_crs(CRS)
 
 fig, ax = plt.subplots(figsize=(15, 10))
 ams_nb.plot(ax=ax)
@@ -75,7 +76,10 @@ for label in metro_labels:
 
 #%% Overlay the grid over the neighborhoods to calculate population by grid
 # https://gis.stackexchange.com/questions/421888/getting-the-percentage-of-how-much-areas-intersects-with-another-using-geopandas
+
 grid['area_grid'] = grid.area
+if CRS == 'EPSG:28992':
+    grid['area_grid_km'] = grid['area_grid'] / 10**6
 ams_nb['area_nb'] = ams_nb.area
 
 overlay = grid.overlay(ams_nb, how='intersection')
@@ -116,6 +120,8 @@ gridinc = weighted_average(overlay_pct, 'avg_inc_per_res', 'area_overlay_pct', '
 # Merge gridpop with the original grid
 grid = grid.merge(gridpop, on='v', how='left')
 grid = grid.merge(gridinc, on='v', how='left')
+# Population Density per square
+grid['pop_density_km'] = grid['grid_pop'] / grid['area_grid_km']
 # grid['grid_pop'] = grid['grid_pop'].fillna(0)
 grid.to_csv('./amsterdam_grid.csv', index=False)
 
@@ -139,9 +145,10 @@ for i, l in enumerate(metro_lines):
 
 fig.suptitle('Amsterdam Grid Population - Existing Lines', fontsize=30)
 ax.legend()
+fig.savefig(f'./amsterdam_env_{len(rows)}x{len(cols)}_population.png')
 
 fig, ax = plt.subplots(figsize=(15, 10))
-ax.imshow(gridenvinc)
+ax.imshow(gridenvinc, cmap='Blues')
 markers = itertools.cycle(['o','s','v', '^', 'P', 'h'])
 for i, l in enumerate(metro_lines):
     l_v = l.sjoin(grid)
@@ -151,7 +158,7 @@ for i, l in enumerate(metro_lines):
 
 fig.suptitle('Amsterdam Grid Avg Income - Existing Lines', fontsize=30)
 ax.legend()
-
+fig.savefig(f'./amsterdam_env_{len(rows)}x{len(cols)}_avg_income.png')
 
 # %% Print labels of the Grid
 fig, ax = plt.subplots(figsize=(15, 10))
@@ -162,4 +169,48 @@ grid.boundary.plot(ax=ax, edgecolor='gray')
 for idx, row in grid.iterrows():
     plt.annotate(text=row['v'], xy=row['coords'],
                  horizontalalignment='center', fontsize=8)
+# %% Calculate OD Flows using the Universal Mobility Law
+d = 7
+fmin = 1/d
+fmax = d
+od_mx = np.zeros((grid.shape[0], grid.shape[0]))
+for i,row_i in grid.iterrows():
+    for j, row_j in grid.iterrows():
+        if i == j:
+            continue
+        
+        # destination attractiveness
+        mu_j = row_j['pop_density_km'] * row_j['area_grid_km'] ** 2 * fmax
+        
+        if np.isnan(mu_j):
+            mu_j = 0
+        
+        # Manhattan distance
+        r_ij = abs(row_i['g_x'] - row_j['g_x']) + abs(row_i['g_y'] - row_j['g_y'])
+        if np.isnan(r_ij):
+            print(f'Distance between {i} and {j} is nan - this is a bug and should not happen')
+        # Origin Destination flow estimate
+        od_ij = mu_j * row_i['area_grid_km'] / r_ij ** 2 * np.log(fmax/fmin)
+        od_mx[i, j] = od_ij
+
+
+# %% Plot aggregate OD flow for each grid cell
+agg_od_g = np.zeros((grid_x_size, grid_y_size))
+agg_od_v = od_mx.sum(axis=1)
+# Get the grid indices.
+for i in range(agg_od_v.shape[0]):
+    agg_od_g[grid.iloc[i]['g_x'], grid.iloc[i]['g_y']] = agg_od_v[i]
+
+fig, ax = plt.subplots(figsize=(15, 10))
+ax.imshow(agg_od_g, cmap='Blues')
+fig.suptitle('Amsterdam Agregate OD flows', fontsize=30)
+fig.savefig(f'./amsterdam_env_{len(rows)}x{len(cols)}_agg_od_flows.png')
+
+# %% Plot correlation between population and aggregate d
+fig, ax = plt.subplots(figsize=(7, 5))
+grid['aggregate_od'] = agg_od_v
+corr = grid[['grid_pop', 'aggregate_od']].corr().iloc[0, 1]
+grid.plot.scatter('grid_pop', 'aggregate_od', ax=ax)
+fig.suptitle(f'Aggregate OD flows vs Population: Pearson: {round(corr, 3)}')
+fig.savefig(f'./amsterdam_env_{len(rows)}x{len(cols)}_agg_od_vs_pop.png')
 # %%
