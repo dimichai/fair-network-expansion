e55898e3c5cefad48e0516ed769fd277ea039afc
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2022-07-09 16:44:01 +0200

    one lr

--- hparam_search.sh
+++ hparam_search.sh
@@ -4,18 +4,18 @@ ARCHS=("mlp")
 SEEDS=(20)
 # ACTOR_LAY=(9)
 # CRITIC_LAY=(2 3 4 5 6)
-ACTOR_LAY=(1)
+ACTOR_LAY=(3)
 CRITIC_LAY=(2)
 # ACTOR_LR=(0.001 0.0005 0.0001 0.00005)
 # CRITIC_LR=(0.001 0.0005 0.0001 0.00005)
-ACTOR_LR=(0.001)
-CRITIC_LR=(0.001)
-EPOCH_MAX=1
+ACTOR_LR=(0.01)
+# CRITIC_LR=(0.001)
+EPOCH_MAX=300
 
 METRO_ENV_L=(diagonal_5x5 dilemma_5x5 xian amsterdam)
-METRO_ENV=${METRO_ENV_L[0]}
+METRO_ENV=${METRO_ENV_L[2]}
 
-PRINT_ONLY=0
+PRINT_ONLY=1
 
 
 
@@ -28,15 +28,15 @@ do
     do
         for clay in ${CRITIC_LAY[@]}
         do
-            for alr in ${ACTOR_LR[@]}
+            for lr in ${ACTOR_LR[@]}
             do 
-                for clr in ${CRITIC_LR[@]}
-                do
+                # for clr in ${CRITIC_LR[@]}
+                # do
                     for seed in ${SEEDS[@]}
                     do
                         if [ $PRINT_ONLY -eq 0 ]; then
                             d=`date +"%d-%b-%Y %T"`
-                            txt="|$d| epochs=$EPOCH_MAX - $arch $alay $clay $alr $clr ($seed):\t"
+                            txt="|$d| epochs=$EPOCH_MAX - $arch $alay $clay $lr ($seed):\t"
                             echo -n -e $txt | tee -a hps_"$METRO_ENV"_$arch.txt
                         fi
                         cmd="python main.py --environment $METRO_ENV \
@@ -44,8 +44,8 @@ do
                                             --seed $seed \
                                             --arch $arch \
                                             --epoch_max $EPOCH_MAX \
-                                            --actor_lr $alr \
-                                            --critic_lr $clr \
+                                            --actor_lr $lr \
+                                            --critic_lr $lr \
                                             --actor_mlp_layers $alay \
                                             --critic_mlp_layers $clay"
                         if [ $PRINT_ONLY -eq 0 ]; then
@@ -59,7 +59,7 @@ do
                             echo $cmd
                         fi
                     done
-                done
+                # done
             done
         done
     done
