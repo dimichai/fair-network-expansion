corrected calculation of average distance to nearest pt stop

trainer.py
@@ -285,16 +285,18 @@ class Trainer(object):
             # Evaluate average distance to nearest public transport station.
             # Manhattan distance between each grid cell and the average generated line.
             line_g = self.environment.vector_to_grid(line).transpose(0, 1)
+            # Calculate the distance from every grid cell to every station in the generated line.
             dist = torch.cdist(self.environment.grid_indices.float(), torch.tensor(line_g).float(), p=1)
+            # Find the distance of the station closest to each cell. Reshape as the grid (grid_x x grid_y)
             min_dist = dist.min(axis=1)[0].reshape_as(self.environment.grid_groups)
             # average distance of all grids with an assigned group (not NaN)
-            distances[i] = (~self.environment.grid_groups.isnan() * min_dist).mean()
+            distances[i] = (~self.environment.grid_groups.isnan() * min_dist).sum() / (~self.environment.grid_groups.isnan()).sum()
 
             if self.environment.group_od_mx:
                 for j, g_od in enumerate(self.environment.group_od_mx):
                     satisfied_group_ods[i, j] = (g_od * sat_od_mask).sum().item()
 
-                    group_distances[i, j] = ((self.environment.grid_groups == self.environment.groups[j]) * min_dist).mean()
+                    group_distances[i, j] = ((self.environment.grid_groups == self.environment.groups[j]) * min_dist).sum() / (self.environment.grid_groups == self.environment.groups[j]).sum()
 
         mean_sat_od = satisfied_ods.mean()
         mean_sat_od_pct = mean_sat_od / (self.environment.od_mx.sum() / 2)
@@ -313,6 +315,14 @@ class Trainer(object):
 
         fig.savefig(Path(args.result_path, 'satisfied_od_by_group.png'))
         # log_artifact(Path(args.result_path, 'satisfied_od_by_group.png'))
+
+        # Plot bars of average distance to nearest stop by group and overall
+        fig, ax = plt.subplots(figsize=(7, 5))
+        ax.bar(range(mean_group_distance.shape[0]), mean_group_distance)
+        ax.title.set_text(f'Mean distance to nearest stop by group \n Mean Overall Distance: {round(mean_distance, 2)} \n Model: {args.result_path}')
+        ax.axhline(y=mean_distance,linewidth=1, color='gray', ls='--')
+
+        fig.savefig(Path(args.result_path, 'mean_distance_to_stop.png'))
 
         group_gini = gini(mean_sat_od_by_group)
         group_pct_gini = gini(mean_sat_od_by_group_pct)