a1f86139d378b4ecb12a047b92f76e717dbc3bee
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2022-07-24 11:38:21 +0200

    Rnn hparam search

--- hparam_search.sh
+++ hparam_search.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-ARCHS=("mlp")
-SEEDS=(20)
-# ACTOR_LAY=(9)
-# CRITIC_LAY=(2 3 4 5 6)
-ACTOR_LAY=(1)
-CRITIC_LAY=(2)
+ARCHS=("rnn")
+SEEDS=(20 303 5789)
+ACTOR_LAY=(2 3 4 5 6)
+CRITIC_LAY=(1 2 3 4 5)
 # ACTOR_LR=(0.001 0.0005 0.0001 0.00005)
 # CRITIC_LR=(0.001 0.0005 0.0001 0.00005)
-ACTOR_LR=(0.001)
-CRITIC_LR=(0.001)
-EPOCH_MAX=1
+ACTOR_LR=(0.0001)
+EPOCH_MAX=500
 
 METRO_ENV_L=(diagonal_5x5 dilemma_5x5 xian amsterdam)
 METRO_ENV=${METRO_ENV_L[0]}
 
 PRINT_ONLY=0
-
-
 
 for arch in ${ARCHS[@]}
 do
@@ -30,13 +25,17 @@ do
         do
             for alr in ${ACTOR_LR[@]}
             do 
-                for clr in ${CRITIC_LR[@]}
-                do
+                if [ $alay -gt $clay ]; then
+                # diff=$alay-$clay
+                # echo $diff
+                if [[ $((alay-clay)) -lt 4 ]]; then
+                # for clr in ${CRITIC_LR[@]}
+                # do
                     for seed in ${SEEDS[@]}
                     do
                         if [ $PRINT_ONLY -eq 0 ]; then
                             d=`date +"%d-%b-%Y %T"`
-                            txt="|$d| epochs=$EPOCH_MAX - $arch $alay $clay $alr $clr ($seed):\t"
+                            txt="|$d| epochs=$EPOCH_MAX - $arch $alay $clay $alr $alr ($seed):\t"
                             echo -n -e $txt | tee -a hps_"$METRO_ENV"_$arch.txt
                         fi
                         cmd="python main.py --environment $METRO_ENV \
@@ -45,9 +44,10 @@ do
                                             --arch $arch \
                                             --epoch_max $EPOCH_MAX \
                                             --actor_lr $alr \
-                                            --critic_lr $clr \
+                                            --critic_lr $alr \
                                             --actor_mlp_layers $alay \
-                                            --critic_mlp_layers $clay"
+                                            --critic_mlp_layers $clay \
+                                            --early_stopping 20"
                         if [ $PRINT_ONLY -eq 0 ]; then
                             eval "$cmd 2>/dev/null \
                                     | grep 'Average.*flows:\|Number' \
@@ -59,7 +59,9 @@ do
                             echo $cmd
                         fi
                     done
-                done
+                # done
+                fi
+                fi
             done
         done
     done
