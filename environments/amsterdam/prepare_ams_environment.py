180a8d0fc5ccab2c41fb77faeaccd6b379d76dd8
dimichai <9805570+dimichai@users.noreply.github.com>
2023-10-20 17:53:32 +0200

    added new amstedam_10x10 environment & code to generate (commented out)

--- environments/amsterdam/prepare_ams_environment.py
+++ environments/amsterdam/prepare_ams_environment.py
@@ -17,7 +17,7 @@ from environment import Environment
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-house_price_bins = 10
+house_price_bins = 5
 
 if __name__ == "__main__":
     # Note: Run this file
@@ -50,4 +50,44 @@ if __name__ == "__main__":
     plt.savefig(env_path / f'house_price_groups_{house_price_bins}.png', bbox_inches='tight')
 
 
-# %%
+# %% Create Amsterdam-10x10 environment, a 10x10 grid that is a subset of the original environment
+# from itertools import product
+# environment = Environment(Path(f"./environments/amsterdam"))
+# row_indices = np.arange(12, 22)
+# col_indices = np.arange(10, 20)
+# # cells = np.array([[row_indices[i], col_indices[i]] for i in range(len(row_indices))])
+# cells = list(product(row_indices, col_indices))
+# indices = environment.grid_to_vector(np.array(cells))
+
+# price_mx_small = environment.price_mx[row_indices[:, np.newaxis], col_indices]
+# price_mx_small_binned = np.digitize(price_mx_small, bins).astype(np.float32)
+# # price_mx_small_binned[np.isnan(price_mx_small)] = np.nan
+# od_mx_small = environment.od_mx[indices][:, indices]
+# od_mx_small = od_mx_small/od_mx_small.sum()
+
+# fig, ax = plt.subplots(figsize=(5, 5))
+# im = ax.imshow(price_mx_small_binned)
+# environment.existing_lines = []
+# environment.existing_lines_full = []
+
+# # Create directory 
+# env_path = Path(f"./environments/amsterdam_10x10")
+# env_path.mkdir(parents=True, exist_ok=True)
+# # Write the file od.txt with the OD matrix
+# with open(env_path / f'./od.txt', 'w+') as f:
+#     for i in range(od_mx_small.shape[0]):
+#         for j in range(od_mx_small.shape[1]):
+#             if od_mx_small[i, j] >= 0:
+#                 f.write(f'{i},{j},{od_mx_small[i,j]}\n')
+# # Write the file average_house_price_gid.txt with the average house price per group
+# with open(env_path / f'./average_house_price_gid.txt', 'w+') as f:
+#     for i in range(price_mx_small.shape[0]):
+#         for j in range(price_mx_small.shape[1]):
+#             if not np.isnan(price_mx_small[i, j]):
+#                 f.write(f'{i},{j},{price_mx_small[i,j]}\n')
+# # Write the file price_groups_2.txt with the group membership of each square
+# with open(env_path / f'./price_groups_{house_price_bins}.txt', 'w+') as f:
+#     for i in range(price_mx_small_binned.shape[0]):
+#         for j in range(price_mx_small_binned.shape[1]):
+#             if not np.isnan(price_mx_small_binned[i, j]):
+#                 f.write(f'{i},{j},{price_mx_small_binned[i,j]}\n')
